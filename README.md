41cfc5acd59c8476f49cd5d82f0957e7e7fee698
ayunami2000 <spwilliamsiam@gmail.com>
2022-04-18 18:56:09 -0400

    add initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# ayunFreedomMod
+
+a new free op experience, for 1.5.2!
+
+**Inspiration: TotalFreedomMod** (*play.totalfreedom.me*)
---
+++ src/main/java/me/ayunami2000/ayunFreedomMod/Main.java
@@ -0,0 +1,15 @@
+package me.ayunami2000.ayunFreedomMod;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
